209f7ab9e80c064f234670b4db7b061ec239be2e
dagnelies <arnaud.dagnelies@gmail.com>
2022-11-11 09:45:06 +0100

    ...

--- js/sign-in-with.js
+++ js/sign-in-with.js
@@ -1,30 +1,30 @@
 async function init() {
-	// The API call to fetch the user
+  // The API call to fetch the user
   const res = await fetch('https://api.passwordless.id/userinfo', {
     mode: 'cors',
     credentials: 'include'
   })
-  
+
   if (res.ok) {
-  	// Display it
+    // Display it
     const userinfo = await res.json()
     showUser(userinfo)
-  } else if(res.status === 401) {
-  	// User must first sign in (or create account)
+  } else if (res.status === 401) {
+    // User must first sign in (or create account)
     // It will be redirected back here once done
     document.getElementById('login').className = 'visible'
   }
   else {
-  	console.warn(res)
-  	alert(`Unexpected Error: ${res.status} ${await res.text()}`)
+    console.warn(res)
+    alert(`Unexpected Error: ${res.status} ${await res.text()}`)
   }
 }
 
 function showUser(userinfo) {
-	document.querySelector('#userinfo img').src = userinfo.picture
-    document.querySelector('#userinfo h1').textContent = userinfo.nickname ?? 'Error: username missing?!'
-    document.querySelector('#userinfo pre').textContent = JSON.stringify(userinfo, null, 2)
-    document.getElementById('userinfo').className = 'visible'
+  document.querySelector('#userinfo img').src = userinfo.picture
+  document.querySelector('#userinfo h1').textContent = userinfo.nickname ?? 'Error: username missing?!'
+  document.querySelector('#userinfo pre').textContent = JSON.stringify(userinfo, null, 2)
+  document.getElementById('userinfo').className = 'visible'
 }
 
 init()
